a73b2142f99f76694253f819c86393c2cb5ada45
ashu <ashutosh.kuma_cs18>
2021-05-10 01:27:39 +0530

    add api key, api url, image url

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_KEY = '927add11553bcfabc08eee589931d54f';
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+export const IMAGE_URL = 'http://image.tmdb.org/t/p/';
