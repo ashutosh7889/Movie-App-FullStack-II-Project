f164b43fd99cab3da2b383c26e0b96f0530eb661
ashu <ashutosh.kuma_cs18>
2021-05-10 01:28:42 +0530

    adding if elese statement

--- client/src/components/views/LandingPage/Sections/GridCard.js
+++ client/src/components/views/LandingPage/Sections/GridCard.js
@@ -2,15 +2,26 @@ import React from 'react';
 import { Col } from 'antd';
 
 function GridCard(props) {
-    return (
-        <Col lg={6} md={8} xs={24}>
-            <div style={{ position: 'relative'}}>
-                <a href={`/movie/${props.movieId}`} >
+    if(props.actor) {
+        return (
+            <Col lg={6} md={8} xs={24}>
+                <div style={{ position: 'relative'}}>
                         <img style={{ width: '100%', height: '320px' }} alt="img" src={props.image} />
-                </a>
-            </div>
-        </Col>
-    )
+                </div>
+            </Col>
+        )
+    } else {
+        return (
+            <Col lg={6} md={8} xs={24}>
+                <div style={{ position: 'relative'}}>
+                    <a href={`/movie/${props.movieId}`} >
+                            <img style={{ width: '100%', height: '320px' }} alt="img" src={props.image} />
+                    </a>
+                </div>
+            </Col>
+        )
+    }
+    
 }
 
 export default GridCard
