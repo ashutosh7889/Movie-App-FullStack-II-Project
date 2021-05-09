6cf59eff397bd4e7d41d1db557b5bc870ffb9f23
ashu <ashutosh.kuma_cs18>
2021-05-09 21:41:25 +0530

    creating grids banners fro landing page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,66 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+// import { response } from 'express';
+import React, {useEffect, useState} from 'react'
+import { API_URL, API_KEY } from '../../Config';
+import { Typography, Row } from 'antd';
+const { Title } = Typography;
 
 function LandingPage() {
+
+    const [Movies, setMovies] = useState([])
+
+    useEffect(() => {
+        fetch(`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+            .then(response=> response.json())
+            .then(response => {
+                console.log(response)
+                setMovies(response.results)
+            })
+    }, [])
+
+
     return (
         <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+            <div style={{width:'100%', margin:0}} >
+                {/* Movie Main Image  */}
+                <div style={{ background:
+                        `linear-gradient(to bottom, rgba(0,0,0,0)
+                39%,rgba(0,0,0,0)
+                41%,rgba(0,0,0,0.65)
+                100%),
+                url(''),#1c1c1c`,
+                    height:'500px',
+                    backgroundSize:'100%, cover',
+                    backgroundPosition: 'center, center',
+                    width:'100%',
+                    position: 'relative' }}>
+
+                </div>
+
+                <div>
+                    <div style={{ position: 'absolute', maxWidth: '500px', bottom: '2rem', marginLeft: '2rem' }}>
+                        <Title style={{ color: 'white' }} level={2} > Title</Title>
+                        <p style={{ color:'white', fontSize: '1rem'}}>text</p>
+                    </div>
+                </div>
+
+                {/* Body */}
+                <div style={{ width: '85%', margin: '1rem auto'}} >
+                    <Title level={2} > Movies by latest</Title>
+                    <hr />
+
+                    {/* Grid Card */}
+                    <Row gutter={[16, 16]}>
+
+                    </Row>
+
+                    {/* Load More Button  */}
+                    <br />
+                    <div style={{ display:'flex', justifyContent: 'center'}} >
+                        <button onClick> Load More </button>
+                    </div>
+
+                </div>
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
         </>
     )
 }
