6a170901663d969ce8be78943689ef964e87bd6f
ashu <ashutosh.kuma_cs18>
2021-05-10 21:02:21 +0530

    footer description changed

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
             flexDirection: 'column', alignItems: 'center',
             justifyContent: 'center', fontSize:'1rem'
         }}>
-           <p> Happy Coding  <Icon type="smile" /></p>
+           <p> 2021 ASHUTOSH KUMAR AND MAYANK KUMAR SINGH  <Icon type="smile" /></p>
         </div>
     )
 }
