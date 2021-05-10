2e23f6618a37fc7e05a08fe7b16b4c48fe3e2d5e
ashu <ashutosh.kuma_cs18>
2021-05-10 20:47:25 +0530

    remove home menu from the navbar

--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -6,9 +6,6 @@ const MenuItemGroup = Menu.ItemGroup;
 function LeftMenu(props) {
   return (
     <Menu mode={props.mode}>
-    <Menu.Item key="mail">
-      <a href="/">Home</a>
-    </Menu.Item>
     <Menu.Item key="favorite">
       <a href="/favorite">Favorite</a>
     </Menu.Item>
